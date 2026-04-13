48d5e2d3593f1212a1f162c2c873a169886ab72b
jstrutzel01 <jstrutzel01@csu.fullerton.edu>
2026-04-13 16:42:15 -0700

    Removed trailing slash in amplify url

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -154,7 +154,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://main.d2wjmdcvmn0xbr.amplifyapp.com/',  #Amplify URL
+    'https://main.d2wjmdcvmn0xbr.amplifyapp.com',  #Amplify URL
 ]
 
 CORS_ALLOW_CREDENTIALS = True
