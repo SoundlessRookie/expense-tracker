5c2f6d0125b0c84ec77fbee510b753af9e28ba56
jstrutzel01 <jstrutzel01@csu.fullerton.edu>
2026-04-27 16:07:18 -0700

    added cognito credentials

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -160,3 +160,9 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+COGNITO_REGION = os.environ.get('COGNITO_REGION', 'us-east-1')
+COGNITO_USER_POOL_ID = os.environ.get('COGNITO_USER_POOL_ID', '')
+COGNITO_APP_CLIENT_ID = os.environ.get('COGNITO_APP_CLIENT_ID', '')
+
+
