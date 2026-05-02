641b8deaaf8ae05cb74ef0549a20328227322aa1
jstrutzel01 <jstrutzel01@csu.fullerton.edu>
2026-05-02 02:47:47 -0700

    Added custom domain to CORS_ALLOWED ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -154,7 +154,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://main.d2wjmdcvmn0xbr.amplifyapp.com',  #Amplify URL
+    'https://wallyexpensetracker.com',
+    'https://www.wallyexpensetracker.com',
 ]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
